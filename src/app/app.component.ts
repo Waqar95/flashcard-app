3fb86cd776d6fad0b50889c9accbd6d26ebe7572
waqar95 <waqar95ahmed@gmail.com>
2025-07-18 16:52:35 +0200

    Add navigation header with links to Review and Create pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterModule, HeaderComponent],
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'flashcard-app';
