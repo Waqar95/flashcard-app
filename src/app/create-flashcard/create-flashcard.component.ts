e7d65180b315a635c9d265c0767c2b11810ab4bc
waqar95 <waqar95ahmed@gmail.com>
2025-07-18 12:46:50 +0200

    Add create Flashcard form with Reactive Forms and submit logic

--- src/app/create-flashcard/create-flashcard.component.ts
+++ src/app/create-flashcard/create-flashcard.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { FlashcardService } from '.././services/flashcard.service';
 
 @Component({
   selector: 'app-create-flashcard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-flashcard.component.html',
-  styleUrl: './create-flashcard.component.css'
+  styleUrls: ['./create-flashcard.component.css'],
 })
 export class CreateFlashcardComponent {
+  flashcardForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private flashcardService: FlashcardService
+  ) {
+    // Initialize the form
+    this.flashcardForm = this.fb.group({
+      question: ['', Validators.required],
+      answer: ['', Validators.required],
+      topic: [''],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.flashcardForm.valid) {
+      this.flashcardService.add(this.flashcardForm.value);
+      alert('Flashcard added!');
+      this.flashcardForm.reset();
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [...appConfig.providers, provideHttpClient()],
+});
